acc097f90c82c09d8395c0663a996a915ff2998f
Az <N/A>
2022-05-23 00:56:31 +0100

    Live translation mode

--- az/interpreter.py
+++ az/interpreter.py
@@ -1,4 +1,6 @@
-import sys # Import system
+import sys
+from tokenisation.utils import writeTokens
+from tokenisation.tokeniser import loopSourceAndGenerateTokens # Import system
 
 from templates import Templates # Language templates
 from code_generators.generate_csharp import code_generation # C# code generator
@@ -17,6 +19,26 @@ tokenised_source = ""
 # Generate code
 def build_code():
     code_generation()
+
+def live_code_translation():
+    print("Type 'exit' to quit!")
+    while True:
+        val = input(">>> ")
+        if val == "exit":
+            break
+        else:
+            tokenised_source_generator = loopSourceAndGenerateTokens(val)
+            tokenised_source = ""
+            for token in tokenised_source_generator:
+                tokenised_source += token + ", "
+            print("\n\n")
+            print(f"Tokens:\n{tokenised_source}")
+            print("\n\nC# code:")
+            build_code()
+
+if (sys.argv[1] == "--live"):
+    live_code_translation()
+    exit(0)
 
 with open(sourcefile, "r") as f:
     # read file contains
--- az/test.cs
+++ az/test.cs
@@ -1,24 +1,2 @@
-// Test single line comment
-static  void main(){
-{
-print("Hello, Knots!\nAwoos~\n");
-// Test single line comment
-const float add = adder(5, 6);
-const float minus = subtractor(5, 6);
-const float mult = multiplier(5, 6);
-const float div = divider(5, 6);
-}
-}
-static  double adder(double a, double b){
-return a + b;
-}
-static  double subtractor(double a, double b){
-return a - b;
-}
-static  double multiplier(double a, double b){
-return a * b;
-}
-static  double divider(double a, double b){
-return a / b;
-}
- static void print(string message) => Console.Write(message);
+Input code:
+>>> 
