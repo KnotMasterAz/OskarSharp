1bf0e786ba72ccff16a2c9fb3eeacbf826ef5755
Az <N/A>
2022-05-22 04:30:14 +0100

    Replaced built-in test file with required file as an argument

--- az/interpreter.py
+++ az/interpreter.py
@@ -1,18 +1,22 @@
+import sys # Import system
+
 from templates import Templates # Language templates
 from code_generators.generate_csharp import code_generation # C# code generator
 
-from tokenisation.tokeniser import tokeniser
+from tokenisation.tokeniser import tokeniser # Import tokeniser
+
+if sys.argv.__len__() < 2:
+    print("Usage: python3 interpreter.py <file>")
+    exit(1)
 
 # Source files
-sourcefile = "test.oskar"
+sourcefile = sys.argv[1]
 source = ""
 tokenised_source = ""
 
 # Generate code
 def build_code():
     code_generation()
-
-
 
 with open(sourcefile, "r") as f:
     # read file contains
