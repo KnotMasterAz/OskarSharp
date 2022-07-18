89093168004322c0b605b46289c5b02ff624d453
Az Foxxo <63619429+KnotMasterAz@users.noreply.github.com>
2022-07-18 07:34:16 +0100

    Slightly cleaned up project structure

--- az/src/Program.cs
+++ az/src/Program.cs
@@ -7,6 +7,20 @@ namespace Az
     {
         static void Main(string[] args)
         {
+            // Check if the user provided a file to parse
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: Az.exe <file>");
+                return;
+            }
+            // Check the file exists, and if not, exit.
+            if (!System.IO.File.Exists(args[0]))
+            {
+                Console.WriteLine("File not found: " + args[0]);
+                return;
+            }
+            
+            // Create a new tokeniser and parse the file.
             Lexer lexer = new Lexer();
             lexer.Lex(@"./test/main.oskar");
         }
